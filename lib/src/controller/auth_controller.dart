a2f8d3be5a1eb5df8dff191b8b494ea2a180cbb2
apple <adil.ayoub@primotech.com>
2023-01-06 11:47:49 +0530

    apple login

--- lib/src/controller/auth_controller.dart
+++ lib/src/controller/auth_controller.dart
@@ -1,63 +1,60 @@
+import 'package:dog_news/feature/apple_login.dart';
 import 'package:dog_news/feature/facebook_login.dart';
 import 'package:dog_news/feature/google_login.dart';
 import 'package:dog_news/src/UI/card/shared_pref.dart';
-import 'package:dog_news/src/UI/setting/setting_page.dart';
 import 'package:dog_news/src/controller/setting_controller.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../utils/localization/localization_String.dart';
 import '../../utils/routes/app_routes.dart';
 
-class SignController extends GetxController{
-SettingController settingController = Get.find();
+class SignController extends GetxController {
+  SettingController settingController = Get.find();
   User? user;
-  Function onClickFunction({required OnClick action}){
-    Map<OnClick ,void Function()> actions = {
+  Function onClickFunction({required OnClick action}) {
+    Map<OnClick, void Function()> actions = {
       /// Click  facebook
-      OnClick.facebook : () async{
-
-        user =await FacebookManager().signInWithFacebook();
-        if(user != null){
+      OnClick.facebook: () async {
+        user = await FacebookManager().signInWithFacebook();
+        if (user != null) {
           SharePreference.addStringToSF(LocalString.signKey, "${user!.email!}");
           settingController.getGoogleLoginValue();
           Get.back();
         }
       },
+
       /// Click Google
-      OnClick.google : () async{
-        user =await LoginManager().signInWithGoogle();
-         if(user != null){
-          await SharePreference.addStringToSF(LocalString.signKey, "${user!.email!}");
+      OnClick.google: () async {
+        user = await LoginManager().signInWithGoogle();
+        if (user != null) {
+          await SharePreference.addStringToSF(
+              LocalString.signKey, "${user!.email!}");
           settingController.getGoogleLoginValue();
           Get.back();
-
-         }
+        }
       },
-      /// Click Twitter
-      OnClick.twitter : (){print(action);},
-      /// Click phone
-      OnClick.phone : (){
 
+      /// Click Twitter
+      OnClick.twitter: () async {
+        user = await AppleLoginService().signInWithApple();
+        if (user != null) {
+          await SharePreference.addStringToSF(
+              LocalString.signKey, "${user!.email!}");
+          settingController.getGoogleLoginValue();
+          Get.back();
+        }
+      },
+
+      /// Click phone
+      OnClick.phone: () {
         Get.toNamed(AppRoutes.phoneAuth);
       },
-
     };
 
     Function act = actions[action]!;
-    return act ;
+    return act;
   }
-
-
-
-
-
 }
 
-enum OnClick{
-  facebook,
-  google,
-  twitter,
-  phone
-}
+enum OnClick { facebook, google, twitter, phone }
